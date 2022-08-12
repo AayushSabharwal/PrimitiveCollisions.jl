fe40db442070268b757459f5ed2442c60656dbe2
Aayush Sabharwal <aayush.sabharwal@gmail.com>
2022-08-12 23:11:57 +0530

    clamp doesn't work with Symbolics

--- src/utils.jl
+++ src/utils.jl
@@ -1,9 +1,11 @@
 @inline sqnorm(pt::SVector) = dot(pt, pt)
 
+ifelseclamp(x, lo, hi) = IfElse.ifelse(x < lo, lo, IfElse.ifelse(x > hi, hi, x))
+
 @inline function point_line_segment_projection(
     p::SVector{2,F}, u::SVector{2,F}, v::SVector{2,F}
 ) where {F}
-    t = clamp(point_line_projection_parameter(p, u, v), zero(F), one(F))
+    t = ifelseclamp(point_line_projection_parameter(p, u, v), zero(F), one(F))
     return u + t * (v - u)
 end
 
@@ -29,10 +31,10 @@ used for edges: `(left, top, right, bottom)`.
 """
 @inline function point_rect_projection(a::Rect, p::SVector{2,R}) where {R}
     return (
-        SVector{2}(R(-a.half_ext[1]), clamp(p[2], -a.half_ext[2], a.half_ext[2])),
-        SVector{2}(clamp(p[1], -a.half_ext[1], a.half_ext[1]), R(a.half_ext[2])),
-        SVector{2}(R(a.half_ext[1]), clamp(p[2], -a.half_ext[2], a.half_ext[2])),
-        SVector{2}(clamp(p[1], -a.half_ext[1], a.half_ext[1]), R(-a.half_ext[2])),
+        SVector{2}(R(-a.half_ext[1]), ifelseclamp(p[2], -a.half_ext[2], a.half_ext[2])),
+        SVector{2}(ifelseclamp(p[1], -a.half_ext[1], a.half_ext[1]), R(a.half_ext[2])),
+        SVector{2}(R(a.half_ext[1]), ifelseclamp(p[2], -a.half_ext[2], a.half_ext[2])),
+        SVector{2}(ifelseclamp(p[1], -a.half_ext[1], a.half_ext[1]), R(-a.half_ext[2])),
     )
 end
 
